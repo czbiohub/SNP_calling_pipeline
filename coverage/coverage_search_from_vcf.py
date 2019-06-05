fbb94f07831c0714ecb4aae01df9335bc879fcd8
Lincoln Harris <lincoln.harris@czbiohub.org>
2019-06-05 14:01:23 -0700

    normalized_cov_df looks good!!

--- coverage/coverage_search_from_vcf.py
+++ coverage/coverage_search_from_vcf.py
@@ -89,34 +89,32 @@ def coverage_search(df):
 		variant_count = int(AD.split(',')[1])
 		total_count = wt_count + variant_count
 
-		ratio = variant_count / total_count	
-		print(ratio)
-		return(ratio)
+		ret = str(variant_count) + ':' + str(total_count)
+		return(ret)
 
 
 
 def ROI_search(row):
 	""" search for given ROI, across all cells """
-
 	start_ = int(row['start_pos'])
 	chrom_ = int(row['chrom'])
 	end_ = int(row['end_pos'])
 	ROI_ = row['outfile']
 
+	print(ROI_)
 	for f in cell_files_list:
 		currCell = f.strip(currPATH + '/vcf/')
-		#print(currCell)
 
 		vcf_ = vcf_to_dataframe(f)
 		vcf_sub = ROI_df_subset(vcf_, chrom_, start_, end_)
     
 		if not vcf_sub.empty:
-			ratio_ = coverage_search(vcf_sub)
+			counts = coverage_search(vcf_sub)
 		else:
-			ratio_ = 0
+			counts = '0:0'
 
 		cellRow = norm_cov_df['cell'] == currCell
-		norm_cov_df.loc[cellRow, ROI_] = ratio_
+		norm_cov_df.loc[cellRow, ROI_] = counts
 
 
 
@@ -131,11 +129,11 @@ cell_files_list = [currPATH + '/vcf/' + s for s in cell_files_list] # list compr
 
 cells_list = []
 for item in cell_files_list: # just want the cell names
-	curr = item.strip('.vcf')
-	cells_list.append(curr)
+	currCell = item.strip(currPATH + '/vcf/')
+	currCell = currCell.strip('.vcf')
+	cells_list.append(currCell)
 
-ROI_df = pd.read_csv('batch_files/coverageBatch_comb.csv')
-#ROI_df = pd.read_csv('/Users/lincoln.harris/code/cerebra/py_notebooks/coverageBatch_v1.csv')
+ROI_df = pd.read_csv('batch_files/coverageBatch_test.csv')
 ROIs = list(ROI_df['outfile']) # define columns list
 ROIs.insert(0,'cell')
 
@@ -143,3 +141,4 @@ norm_cov_df = pd.DataFrame(columns=ROIs) # define normalized cov df!!
 norm_cov_df['cell'] = cells_list
 
 ROI_df.apply(ROI_search, axis=1) # send original ROI dataframe to search func
+norm_cov_df.to_csv('normalized_coverage_df.csv', index=False)
